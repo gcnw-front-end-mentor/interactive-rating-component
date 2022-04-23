deae495373016ff0e714b213619d41f5a2d511c1
gcnw <SkillCatcher@pm.me>
2022-04-23 12:07:22 -0700

    Looks close, but needs to be refined.

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ circles.forEach(circle => {
 })
 
 const changeColor = (selection) => {
-    circleElements.forEach(element => element.style.backgroundColor = '')
+    circleElements.forEach(element => element.style.backgroundColor = '');
+    circleElements.forEach(element => element.style.color = '');
     selection.currentTarget.style.backgroundColor = 'hsl(25, 97%, 53%)';
+    selection.currentTarget.style.color = 'white';
     if(activeCircle === null){
         activeCircle = circleElements.indexOf(selection.currentTarget);
     }
     else{
-        circleElements[activeCircle].style.backgroundColor = 'hsl(217, 12%, 63%)';
+        circleElements[activeCircle].style.backgroundColor = '';
         activeCircle = circleElements.indexOf(selection.currentTarget);
     }
 }
@@ -23,10 +25,19 @@ circleElements.forEach(circle => {
 })
 
 const submitClick = () => {
-    circleElements.forEach(element => element.style.backgroundColor = '')
-    circleElements[activeCircle].style.backgroundColor = 'hsl(25, 97%, 53%)';
-    submitButton.style.backgroundColor = 'white';
-    submitButton.style.color = 'hsl(25, 97%, 53%)';
+    // circleElements.forEach(element => element.style.backgroundColor = '')
+    // circleElements[activeCircle].style.backgroundColor = 'hsl(25, 97%, 53%)';
+    // submitButton.style.backgroundColor = 'white';
+    // submitButton.style.color = 'hsl(25, 97%, 53%)';
+    if(activeCircle == null){
+        alert("Error: No rating selected!")
+    }
+    else {
+        document.getElementsByClassName('mainContainerBefore')[0].style.display = 'none';
+        document.getElementsByClassName('mainContainerAfter')[0].style.display = 'flex';
+        document.getElementById('selectedNumberConfirmation').textContent = 'You selected ' + (activeCircle+1) + ' out of 5';
+    }
+
 }
 
 let submitButton = document.getElementById('submit')
